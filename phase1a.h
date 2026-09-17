a330a29421c45c69adf80787ab0c9ad3053567c0
Johnathan-Alexander <johnathana040@arizona.edu>
2026-09-17 17:41:53 +0000

    Updated phase1a.h file

--- phase1a.h
+++ phase1a.h
@@ -1,18 +1,23 @@
 #include phase1.h
 
 // Struct for Process
-struct process {
+struct Process {
 	char[MAXNAME];
 	int PID;
 	int parent_PID;
 	int priority;
 	int runnable_status;
 	int quit_state;
+} Process;
 
 // Struct for Queues
+struct RunQueue {
+	Process this_process;
+	Process next_process;
+} RunQueue;
 
+// Enqueue Method
+void enqueue_process(Process *aProcess);
 
-// Process Table
-
-
-// Run Queue Array
+// Dequeue Method
+void dequeue_process(Process *aProcess);
