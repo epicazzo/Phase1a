39f0312c407fd6a5535bed84517204a2de46af3a
Johnathan-Alexander <johnathana040@arizona.edu>
2026-09-17 17:45:34 +0000

    Created phase1a.c file

---
+++ phase1a.c
@@ -0,0 +1,13 @@
+#include phase1.h
+#include phase1a.h
+#include <stdlib.io>
+
+void phase1_init() {
+
+}
+
+int spork(char *name, int(*func)(void *), void *arg, int stacksize, int priority) {
+
+}
+
+
