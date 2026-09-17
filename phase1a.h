73049f5419739f5d7ea771fd6a178bc1476a21c4
Johnathan-Alexander <johnathana040@arizona.edu>
2026-09-17 17:10:44 +0000

    Initializing Git repo

---
+++ phase1a.h
@@ -0,0 +1,18 @@
+#include phase1.h
+
+// Struct for Process
+struct process {
+	char[MAXNAME];
+	int PID;
+	int parent_PID;
+	int priority;
+	int runnable_status;
+	int quit_state;
+
+// Struct for Queues
+
+
+// Process Table
+
+
+// Run Queue Array
